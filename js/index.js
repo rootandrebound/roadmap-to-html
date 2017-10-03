db985300860fe33cc18963bc31b3ef6c8c5ab830
ashleyvernon <avernon526@gmail.com>
2017-10-02 20:13:53 -0700

    fixes navigation and styling across entire site

--- js/index.js
+++ js/index.js
@@ -24,19 +24,12 @@ function initializeSearch() {
   );
 }
 
-function px(value){
-  // coerce to string
-  value = value + '';
-  if( value.indexOf('px') === -1) {
-    value += 'px';
-  }
-  return value;
-}
 
 function handleTopPages(topPages){
-  topPages.forEach(function(page){
-    $('ul.top-pages').append(
-      '<li><h4><a href="'+page.url+'">'+page.title+'</a></h4></li>'
+  var veryTopPages = topPages.slice(1, 6);
+  veryTopPages.forEach(function(page){
+    $('.popular-pages__list').append(
+      '<li class="popular-page"><a href="'+page.url+'">'+page.title+'</a></li>'
     );
   })
 }
@@ -49,69 +42,7 @@ function pullTopPages(){
 }
 
 $(function() {
-
-  /*
-  open state:
-    #site-navigation left = 0
-    main left = width
-  closed state:
-    #site-navigation left = -width
-    main left = 0
-  */
-  // Runs as soon as the page loads
-  var isOnSmallScreen = $('body').width() < 900;
-  var doc = $(document);
-  var body = $('body');
-
-  var mainNode = $('main');
-  var sideNav = $('#site-navigation');
-  var sideNavWidth = px(sideNav.css('width'));
-
-  function menuIsOpen(){
-    return (
-      sideNav.css('left') === px(0)
-    ) && (
-      mainNode.css('left') === px(sideNavWidth)
-    );
-  }
-
-  if (isOnSmallScreen === false && menuIsOpen() === false){
-    // if we are on a big screen, open the menu by default
-    openSideMenu();
-  }
-
-  function catchAllClicksAndCloseSideMenu(event){
-    event.stopImmediatePropagation();
-    closeSideMenu();
-  }
-
-  function openSideMenu(){
-    sideNav.animate({'left': px(0)});
-    mainNode.animate({'left': px(sideNavWidth)});
-    if (isOnSmallScreen){
-      // catch any clicks on the main node and use it to trigger collapse
-      mainNode.on('click', catchAllClicksAndCloseSideMenu);
-    }
-  }
-
-  function closeSideMenu(){
-    if (isOnSmallScreen){
-      mainNode.off('click', catchAllClicksAndCloseSideMenu);
-    }
-    sideNav.animate({'left': '-' + px(sideNavWidth)});
-    mainNode.animate({'left': px(0)});
-  }
-
-  doc.on('click', '#js-toggle-menu', function(){
-    if (menuIsOpen()){
-      closeSideMenu();
-    } else {
-      openSideMenu();
-    }
-  });
-
   initializeSearch();
-
   pullTopPages();
 
 });
