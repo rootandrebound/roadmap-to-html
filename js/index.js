9308fcd08922832f7b14f083b0c73c7dc1faca67
Ben Golder <bgolder@codeforamerica.org>
2017-10-09 18:50:02 -0700

    Removed search autocomplete, added search results page
    - removed all algolia autocomplete
    - added a new search page & template
    - changed javascript to make explicit api calls and render results
    - scoped search query and top pages to only run on specific pages

--- js/index.js
+++ js/index.js
@@ -1,27 +1,61 @@
 var ALGOLIA_APP_ID = "ER4XGAZU3H";
 var ALGOLIA_PUBLIC_KEY = "ad6b289aa74181fef926dc6133bfece7";
+var ALGOLIA_INDEX_NAME = "test_ROADMAP";
+var PREFIX = "road-to-html"
+
+
+function getParameterByName(name, url) {
+  // taken from https://stackoverflow.com/a/901144/399726
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+
+function searchResultTemplate(searchResult){
+  return '<li class="search-result">' + 
+           '<h3 class="search-result__title">' +
+            '<a href="' +
+               '/' + PREFIX + '/' + searchResult.path + '/"' +
+              'class="search-result__link">' +
+                searchResult._highlightResult.title.value +
+            '</a>' +
+           '</h3>' +
+         '</li>'
+}
+
 
 function initializeSearch() {
-  var searchInput = $('#search-input input[type="text"]');
   var client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_PUBLIC_KEY);
-  var index = client.initIndex('test_ROADMAP');
-  searchInput.autocomplete(
-    { hint: false },
-    [
-      {
-        source: $.fn.autocomplete.sources.hits(index, { hitsPerPage: 10 }),
-        displayKey: 'title',
-        templates: {
-          suggestion: function(suggestion) {
-            var hrefValue = suggestion.path;
-            var display = suggestion._highlightResult.title.value;
-            var anchorString = '<a href="/roadmap-to-html/' + hrefValue + '">' + display + '</a>';
-            return anchorString;
-          }
+  var index = client.initIndex(ALGOLIA_INDEX_NAME);
+  var search_term = getParameterByName('q');
+  if( search_term ){
+    // from:
+    //   https://www.algolia.com/doc/api-reference/api-methods/search/
+    index.search({ 
+      query: search_term,
+      hitsPerPage: 50,
+      attributesToRetrieve: [
+        'title', 'heading_text', 'path', 'level', 'objectID']
+      },
+      function searchDone(err, content) {
+        if (err) {
+          console.error(err);
+          return;
         }
+        var resultsElement = $('.search-results__list');
+        for (var h in content.hits) {
+          var result = content.hits[h];
+          var renderedResult = searchResultTemplate(result);
+          resultsElement.append($(renderedResult))
       }
-    ]
-  );
+    });
+  }
+
 }
 
 
@@ -34,6 +68,7 @@ function handleTopPages(topPages){
   })
 }
 
+
 function pullTopPages(){
   $.getJSON(
     'https://s3.amazonaws.com/rr-roadmap-top-pages-checker/top_pages.json',
@@ -41,8 +76,19 @@ function pullTopPages(){
   );
 }
 
-$(function() {
-  initializeSearch();
-  pullTopPages();
 
+function callByBodyDataPagePath(path, func){
+  // this looks for a 'data-page-path' attribute on the body tag
+  // and calls the function if the input path strictly matches the value
+  // of that attribute
+  var bodyPagePath = $('body').data('page-path');
+  if (bodyPagePath == path){
+    func();
+  }
+}
+
+
+$(function() {
+  callByBodyDataPagePath('search', initializeSearch);
+  callByBodyDataPagePath('', pullTopPages);
 });
